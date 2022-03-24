010db71580ba46d338c8f54a3c36a467813233cf
Valentini Kožica <valentini.kozica@gmail.com>
2022-03-24 20:38:59 +0100

    Update UPR-LAB8.ps1

--- UPR-LAB8.ps1
+++ UPR-LAB8.ps1
@@ -1,4 +1,4 @@
-﻿# Pokrenite naredbu kojom ćete izlistati sve aktivne procese.
+# Pokrenite naredbu kojom ćete izlistati sve aktivne procese.
 Get-Process
 # Pronađite procese koji zauzimaju više od 20 MB memorije.
 Get-Process | Where-Object {$_.WorkingSet -gt 20000000}
@@ -6,3 +6,9 @@ Get-Process | Where-Object {$_.WorkingSet -gt 20000000}
 Get-Process -Name 'S*'
 # Pronađite proces koji predstavlja komandnu liniju koju ste pokrenuli.
 Get-Process CMD
+# Izlistajte proces i sortirajte ih po zauzetosti memorije od većeg prema manjem
+Get-Process | Sort-Object -Descending WS 
+# Iz komandne linije pokrenite Notepad tekstualni editor
+Start-Process 'C:\windows\system32\notepad.exe' 
+# Ugasite program Notepad iz komandne linije.
+Stop-Process -Name notepad
