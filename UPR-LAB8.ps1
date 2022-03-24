fedf6ada453369c8fca247d9d38d9775e3079bfd
Valentini Kožica <valentini.kozica@gmail.com>
2022-03-24 20:17:37 +0100

    Add files via upload

---
+++ UPR-LAB8.ps1
@@ -0,0 +1,6 @@
+﻿# Pokrenite naredbu kojom ćete izlistati sve aktivne procese.
+Get-Process
+# Pronađite procese koji zauzimaju više od 20 MB memorije.
+Get-Process | Where-Object {$_.WorkingSet -gt 20000000}
+# Pronađite procese čiji naziv počinje sa slovom s.
+Get-Process -Name 'S*'
